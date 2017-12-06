f7bd7079d74b96288eee184ec7b19988f55744e3
Carlos Martin <carlos.martin@tuva.co>
2017-12-06 14:51:59 +0100

    OffiMate: Added Navigation Drawer Activity to MainActivity

--- app/src/main/java/app/android/carlosmartin/offimate/activities/main/MainActivity.java
+++ app/src/main/java/app/android/carlosmartin/offimate/activities/main/MainActivity.java
@@ -1,46 +1,105 @@
 package app.android.carlosmartin.offimate.activities.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import app.android.carlosmartin.offimate.R;
-import app.android.carlosmartin.offimate.application.OffiMate;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView userNameTextView;
-    private TextView userEmailTextView;
-    private TextView userPasswordTextView;
-    private TextView userOfficeTextView;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OffiMate.currentUser.printLog("MainActivity");
+        setTitle("OffiMate");
 
-        this.initUI();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    private void initUI() {
-        this.userNameTextView = findViewById(R.id.textViewMainUserName);
-        String userName = OffiMate.currentUser.getName();
-        String userUID = OffiMate.currentUser.getUid();
-        this.userNameTextView.setText(userName + " - {{" + userUID + "}}");
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-        this.userEmailTextView = findViewById(R.id.textViewMainUserEmail);
-        this.userEmailTextView.setText(OffiMate.currentUser.getEmail());
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-        this.userPasswordTextView = findViewById(R.id.textViewMainUserPassword);
-        this.userPasswordTextView.setText(OffiMate.currentUser.getPassword());
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        this.userOfficeTextView = findViewById(R.id.textViewMainUserOffice);
-        String officeId =  OffiMate.currentUser.getOffice().id;
-        String officeName = OffiMate.currentUser.getOffice().name;
-        String label = officeName + " - {{" + officeId + "}}";
-        this.userOfficeTextView.setText(label);
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_office:
+                break;
+            case R.id.nav_password:
+                break;
+            case R.id.nav_inbox:
+                break;
+            case R.id.nav_send:
+                break;
+            case R.id.nav_log_out:
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
