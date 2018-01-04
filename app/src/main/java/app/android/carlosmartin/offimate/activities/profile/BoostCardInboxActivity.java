d5b518a0d66d0a21db840663359fc858cde644d9
Carlos Martin <carlos.martin@tuva.co>
2018-01-04 10:25:04 +0100

    OffiMate: Layout added for Inbox and Sent Boost Card at user profile

---
+++ app/src/main/java/app/android/carlosmartin/offimate/activities/profile/BoostCardInboxActivity.java
@@ -0,0 +1,15 @@
+package app.android.carlosmartin.offimate.activities.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.android.carlosmartin.offimate.R;
+
+public class BoostCardInboxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boost_card_inbox);
+    }
+}
---
+++ app/src/main/java/app/android/carlosmartin/offimate/activities/profile/BoostCardSentActivity.java
@@ -0,0 +1,15 @@
+package app.android.carlosmartin.offimate.activities.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.android.carlosmartin.offimate.R;
+
+public class BoostCardSentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boost_card_sent);
+    }
+}
